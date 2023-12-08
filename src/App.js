5a0cf8bb219d46217d5a936eaa1cc5027172b3e3
fredkendy <fredkendy9@gmail.com>
2023-12-08 13:24:12 -0300

    integracao backend com firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Card } from '@rneui/base';
+import database from '@react-native-firebase/database';
 
 export default function App() {
+
+  //Armazenar notícias (conteudo que vamos renderizar)
+  const [listaNoticias, setListaNoticias] = React.useState([])
+
+  //Funciona como um construtor, useEffect é executado sempre que o componente é chamado
+  React.useEffect(() => {
+    database()
+      .ref('/noticias')
+      .on('value', snapshot => {
+        const noticias = Object.values(snapshot.val());
+        setListaNoticias(noticias)
+        console.log('Data: ', noticias);
+      });
+  }, [])
+
   return (
     <View style={{padding: 16}}>
       <Text style={{fontWeight: 'bold', fontSize: 22}}>Feed de notícias</Text>
 
       <View style={{marginTop: 16}}>
-        <Card containerStyle={{marginTop: 14}}>
-          <Card.Title style={{fontSize: 18}}>POLÍTICA</Card.Title>
-          <Card.Divider />
-          <Text style={{fontWeight: 'bold', fontSize: 16}}>
-            Guerra na Ucrânia
-          </Text>
-          <Text style={{fontSize: 14}}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            egestas felis vitae sapien viverra, nec porta risus porttitor. Nunc
-            lacinia sed purus id convallis.
-          </Text>
-        </Card>
+        {
+          listaNoticias.map(data =>
+            <Card containerStyle={{marginTop: 14}}>
+              <Card.Title style={{fontSize: 18}}>{data.categoria}</Card.Title>
+              <Card.Divider />
+              <Text style={{fontWeight: 'bold', fontSize: 16}}>
+                {data.titulo}
+              </Text>
+              <Text style={{fontSize: 14}}>
+                {data.conteudo}
+              </Text>
+            </Card>
+          )
+        }
       </View>
     </View>
   );
